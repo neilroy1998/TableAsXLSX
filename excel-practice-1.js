313fc53d2d0e2c94e27118a7e4aded4e1a9fb447
Rajit Roy <rajit.roy@wabtec.com>
2020-08-27 18:25:37 +0530

    Download Btn

--- excel-practice-1.js
+++ excel-practice-1.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
-    let wb = new ExcelJS.Workbook();
-    let ws = wb.addWorksheet("Sheet1");
 
-    ws.getCell('A1').value = 'Hello, World!';
-    ws.getCell('A2').value = 7;
+    $('#btn2').click(function () {
+        let wb = new ExcelJS.Workbook();
+        let ws = wb.addWorksheet("Sheet1");
 
-    // Using window.saveAs rather than fileSaver.saveAs
-    wb.xlsx.writeBuffer()
-        .then(buffer => window.saveAs(new Blob([buffer]), `${Date.now()}_book.xlsx`))
-        .catch(err => console.log('Error writing excel export', err));
+        ws.getCell('A1').value = 'Hello, World!';
+        ws.getCell('A2').value = 7;
+
+        // Using window.saveAs rather than fileSaver.saveAs
+        wb.xlsx.writeBuffer()
+            .then(buffer => window.saveAs(new Blob([buffer]), `Testing_book.xlsx`))
+            .catch(err => console.log('Error writing excel export', err));
+    });
 });
